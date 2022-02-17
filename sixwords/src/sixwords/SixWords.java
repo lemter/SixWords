e8688ef36ca954aaee6d13ad4be0c03a8937c733
lemter <karim0003b@gmail.com>
2022-02-17 13:35:55 +0600

    added main part

---
+++ sixwords/src/sixwords/SixWords.java
@@ -0,0 +1,15 @@
+package sixwords;
+
+import javax.swing.*;
+
+public class SixWords extends JFrame {
+
+    public SixWords() {
+        this.setVisible(true);
+    }
+
+    public static void main(String args[]) {
+
+    }
+
+}
